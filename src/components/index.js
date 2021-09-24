b0a5d735eaba57eddbb4bb49ea3aa9a01d61571b
oubeidthe1 <oubeidallah46@gmail.com>
2021-09-24 21:52:35 +0100

    *

---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+/* eslint-disable eol-last */
+export { default as Homepage }
+from './Homepage';
+export { default as Navbar }
+from './Navbar';
+export { default as News }
+from './News';
+export { default as Cryptocurrencies }
+from './Cryptocurrencies';
+export { default as Exchanges }
+from './Exchanges';
+export { default as CryptoDetails }
+from './CryptoDetails';
+// eslint-disable-next-line
---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoNewsHeaders = {
+    'x-bingapis-sdk': 'true',
+    'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
+    'x-rapidapi-key': 'f07e1db735msh5476bcde339b828p1fbcfejsndcfbbf0efa40',
+};
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
+
+export const cryptoNewsApi = createApi({
+    reducerPath: 'cryptoNewsApi',
+    baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_NEWS_API_URL }),
+    endpoints: (builder) => ({
+        getCryptoNews: builder.query({
+            query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
+        }),
+    }),
+});
+
+// eslint-disable-next-line eol-last
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
